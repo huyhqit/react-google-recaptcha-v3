4acf5a75c8da11cc4df8b1112638c464fdc0f85b
huyho <>
2022-10-05 18:03:02 +0700

    Discard igonore build and dist folder

---
+++ dist/types/example/with-google-recaptcha-example.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import { IWithGoogleReCaptchaProps } from '../src/with-google-recaptcha';
+export declare const WithGoogleRecaptchaExample: React.ComponentType<{
+    children?: {} | null | undefined;
+} & Partial<IWithGoogleReCaptchaProps>>;
+//# sourceMappingURL=with-google-recaptcha-example.d.ts.map
